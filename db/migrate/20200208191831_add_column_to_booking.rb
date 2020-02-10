79364aec4c0042a86538f1cb72637503164b3946
Angelo David <agdavid112191@gmail.com>
2020-02-10 11:53:40 +0800

    Add paid column in migration

--- db/migrate/20200208191831_add_column_to_booking.rb
+++ db/migrate/20200208191831_add_column_to_booking.rb
@@ -2,5 +2,6 @@ class AddColumnToBooking < ActiveRecord::Migration[6.0]
   def change
     add_column :bookings, :number_of_guest, :integer, default: 0
     add_column :bookings, :confirmed, :boolean, default: false
+    add_column :bookings, :paid, :boolean, default: false
   end
 end
