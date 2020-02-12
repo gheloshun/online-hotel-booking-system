f3196712983baccd10ac20ce9d4a60b1f2726c23
Angelo David <agdavid112191@gmail.com>
2020-02-12 16:50:14 +0800

    Fix conflict with rails admin and will paginate gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get 'users/show'
   resources :rooms
   resources :bookings
-
   root to: 'pages#index'
   get '/all_rooms', to: 'pages#rooms'
   devise_for :users
